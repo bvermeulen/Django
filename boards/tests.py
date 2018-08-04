e66a45986cde2efe3ec36d50b0c32000a3651a64
bvermeulen <bruno_vermeulen2001@yahoo.com>
2018-08-04 17:17:55 +0700

    revised test
    includes print statements

--- boards/tests.py
+++ boards/tests.py
@@ -5,19 +5,22 @@ from .models import Board
 
 class HomeTests(TestCase):
     def setUp(self):
-        self.board = Board.objects.create(name='Django', description='Django board.')
+        self.board = Board.objects.create(name='Django', description='Django board')
         url = reverse('home')
         self.response = self.client.get(url)
 
     def test_home_view_status_code(self):
+        print(f'===> T1: response status code = {self.response.status_code}')
         self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
+        print(f'===> T2: view {view.func} = home')
         self.assertEquals(view.func, home)
 
     def test_home_view_contains_link_to_topics_page(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        print(f'===> T3: {self.response} = href {board_topics_url}')
         self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
 
 
@@ -28,19 +31,23 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_view_success_status_code(self):
         url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(url)
+        print(f'===> T4: {url}: response {response.status_code} = 200')
         self.assertEquals(response.status_code, 200)
 
     def test_board_topics_view_not_found_status_code(self):
         url = reverse('board_topics', kwargs={'pk': 99})
         response = self.client.get(url)
+        print(f'===> T5: {url}: response {response.status_code} = 404')
         self.assertEquals(response.status_code, 404)
 
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
+        print(f'===> T6: view {view.func}, board_topics')
         self.assertEquals(view.func, board_topics)
 
     def test_board_topics_view_contains_link_back_to_homepage(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(board_topics_url)
         homepage_url = reverse('home')
+        print(f'===> T7: {board_topics_url}: response {response}, home_page_url {homepage_url}')
         self.assertContains(response, 'href="{0}"'.format(homepage_url))

b7eefc3e25a42c4ec5d4de13ca2baf147f2210ac
bvermeulen <bruno_vermeulen2001@yahoo.com>
2018-09-27 17:16:24 +0700

    27 sept update 17:16

--- noticeboard/noticeboard/settings.py
+++ noticeboard/noticeboard/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
--- noticeboard/noticeboard/urls.py
+++ noticeboard/noticeboard/urls.py
@@ -26,13 +26,17 @@ Including another URLconf
 from django.conf.urls import url
 from django.urls import path
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', accounts_views.signup, name='signup'), 
+    path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'),
+         name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),

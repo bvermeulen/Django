1ac9f9852e96c90fc73664e0ccc4aedb1649bc43
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2018-10-17 14:41:21 +0700

    Update settings.py

--- noticeboard/noticeboard/settings.py
+++ noticeboard/noticeboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ct=sq#%==8v9-l3n35v3#bd4=o06vt8!nc0&h)j9825da01$-f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

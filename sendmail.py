73478a7d894a1f3ba9033c7aa20f0443146e05c6
bvermeulen <bruno_vermeulen2001@yahoo.com>
2019-01-09 17:50:20 +0700

    delete sendmail

--- sendmail.py
+++
@@ -1,8 +0,0 @@
-def send_simple_message():
-    return requests.post(
-        "https://api.mailgun.net/v3/howdiweb.nl/messages",
-        auth=("api", "YOUR_API_KEY"),
-        data={"from": "Excited User <mailgun@YOUR_DOMAIN_NAME>",
-              "to": ["bar@example.com", "YOU@YOUR_DOMAIN_NAME"],
-              "subject": "Hello",
-              "text": "Testing some Mailgun awesomness!"})

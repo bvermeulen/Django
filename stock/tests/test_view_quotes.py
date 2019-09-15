e3ffac583271218ce5bff2c494ccb3634492498e
bvermeulen <bruno_vermeulen2001@yahoo.com>
2019-09-15 19:55:27 +0700

    tests quotes - first commit 15 September

---
+++ stock/tests/test_view_quotes.py
@@ -0,0 +1,89 @@
+from pprint import pprint
+from django.contrib.auth.models import User
+from django.urls import reverse, resolve
+from django.test import TestCase
+from ..views.quotes import QuoteView
+from ..models import Currency, Exchange, Stock, StockSelection, Portfolio
+
+
+class QuestesTests(TestCase):
+    def setUp(self):
+        self.test_user = 'test'
+        self.test_user_pw = '123'
+        self.default_user = 'default_user'
+
+        self.test_user = User.objects.create_user(username=self.test_user,
+                                                  email='test@howdiweb.nl',
+                                                  password=self.test_user_pw)
+
+        self.default_user = User.objects.create_user(username='default_user',
+                                                     email='default@howdiweb.nl',
+                                                     password='456')
+
+        self.usd = Currency.objects.create(currency='USD',
+                                           usd_exchange_rate='1.0')
+
+        self.eur = Currency.objects.create(currency='EUR',
+                                           usd_exchange_rate='0.9')
+
+        self.nyse = Exchange.objects.create(exchange_short='NYSE',
+                                            exchange_long='New York Stock Exchange',
+                                            time_zone_name='CET',)
+
+        self.aex = Exchange.objects.create(exchange_short='AEX',
+                                           exchange_long='Amsterdam AEX',
+                                           time_zone_name='ECT',)
+
+        self.apple = Stock.objects.create(symbol='AAPL',
+                                          company='Apple',
+                                          currency=self.usd,
+                                          exchange=self.nyse,)
+
+        self.rds = Stock.objects.create(symbol='RDSA.AS',
+                                        company='Royal Dutch Shell',
+                                        currency=self.eur,
+                                        exchange=self.aex)
+
+        self.john_portfolio = Portfolio.objects.create(portfolio_name='test_portfolio',
+                                                       user=self.test_user)
+
+        StockSelection.objects.create(stock=self.apple,
+                                      quantity=10,
+                                      portfolio=self.john_portfolio)
+
+        self.default_aex = Portfolio.objects.create(portfolio_name='AEX',
+                                                    user=self.default_user,)
+
+        StockSelection.objects.create(stock=self.rds,
+                                      quantity=1,
+                                      portfolio=self.default_aex)
+
+    def test_quotes_view_status_code(self):
+        response = self.client.get(reverse('stock_quotes'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_quotes_url_resolves_QuoteView(self):
+        view = resolve('/finance/stock_quote/')
+        self.assertEqual(view.func.view_class, QuoteView)
+
+    def test_quote_view_contains_link_to_home(self):
+        response = self.client.get(reverse('stock_quotes'))
+        home_url = reverse('home')
+        self.assertContains(response, f'href="{home_url}"')
+
+    def test_response_contains_csrf_token(self):
+        response = self.client.get(reverse('stock_quotes'))
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_correct_number_input_tags(self):
+        ''' 5 <input> tags to be found:
+            csrf token
+            quote_string
+            markets (AEX, NYSE)
+            Quote submit
+        '''
+        response = self.client.get(reverse('stock_quotes'))
+        self.assertContains(response, '<input', 5)
+
+    def test_valid_quote_string_returns_stock_table(self):
+        pass
--- stock/urls.py
+++ stock/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from stock.views import quote_view, portfolio_view
+from stock.views import quotes, portfolios
 
 urlpatterns = [
-    path('finance/stock_quote', quote_view.QuoteView.as_view(), name='stock_quotes'),
+    path('finance/stock_quote/', quotes.QuoteView.as_view(), name='stock_quotes'),
     path('finance/stock_intraday/<str:symbol>/',
-         quote_view.IntraDayView.as_view(), name='stock_intraday'),
+         quotes.IntraDayView.as_view(), name='stock_intraday'),
     path('finance/stock_history/<str:symbol>/<str:period>/',
-         quote_view.HistoryView.as_view(), name='stock_history'),
+         quotes.HistoryView.as_view(), name='stock_history'),
 
-    path('finance/portfolio/', portfolio_view.PortfolioView.as_view(), name='portfolio'),
+    path('finance/portfolio/', portfolios.PortfolioView.as_view(), name='portfolio'),
 ]

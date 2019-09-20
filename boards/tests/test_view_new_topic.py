b4c4609fe14e6904cbbbdb5abe1e71f484f7e3fa
bvermeulen <bruno_vermeulen2001@yahoo.com>
2019-09-20 10:37:50 +0800

    revise boards test to run with postgres

--- boards/tests/test_view_new_topic.py
+++ boards/tests/test_view_new_topic.py
@@ -6,25 +6,36 @@ from ..forms import NewTopicForm
 from ..models import Board, Topic, Post
 
 class LoginRequiredNewTopicTests(TestCase):
-    def setUp(self):
-        Board.objects.create(name='Django', description='Django board')
+
+    @classmethod
+    def setUpTestData(cls):
+        board = Board.objects.create(name='Django', description='Django board')
+        cls.board_pk = board.pk
 
     def test_redirection(self):
         login_url = reverse('login')
-        url = reverse('new_topic', kwargs={'board_pk': 1})
+        url = reverse('new_topic', kwargs={'board_pk': self.board_pk})
         response = self.client.get(url)
         self.assertRedirects(response, f'{login_url}?next={url}')
 
 
 class NewTopicTests(TestCase):
-    def setUp(self):  # does not run
-        Board.objects.create(name='Django', description='Django board')
+
+    @classmethod
+    def setUpTestData(cls):
+        board = Board.objects.create(name='Django', description='Django board')
         User.objects.create_user(username='john', email='john@doe.com',
                                  password='123')
+        cls.board_pk = board.pk
+
+    def setUp(self):
+        # self.board = Board.objects.create(name='Django', description='Django board')
+        # User.objects.create_user(username='john', email='john@doe.com',
+        #                          password='123')
         self.client.login(username='john', password='123')
 
     def test_new_topic_view_success_status_code(self):
-        url = reverse('new_topic', kwargs={'board_pk': 1})
+        url = reverse('new_topic', kwargs={'board_pk': self.board_pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
@@ -38,24 +49,24 @@ class NewTopicTests(TestCase):
         self.assertEqual(view.func, new_topic)
 
     def test_new_topic_view_contains_link_back_to_board_topics_view(self):
-        new_topic_url = reverse('new_topic', kwargs={'board_pk': 1})
-        board_topics_url = reverse('board_topics', kwargs={'board_pk': 1})
+        new_topic_url = reverse('new_topic', kwargs={'board_pk': self.board_pk})
+        board_topics_url = reverse('board_topics', kwargs={'board_pk': self.board_pk})
         response = self.client.get(new_topic_url)
         self.assertContains(response, f'href="{board_topics_url}"')
 
     def test_csrf(self):
-        url = reverse('new_topic', kwargs={'board_pk': 1})
+        url = reverse('new_topic', kwargs={'board_pk': self.board_pk})
         response = self.client.get(url)
         self.assertContains(response, 'csrfmiddlewaretoken')
 
     def test_contains_form(self):
-        url = reverse('new_topic', kwargs={'board_pk': 1})
+        url = reverse('new_topic', kwargs={'board_pk': self.board_pk})
         response = self.client.get(url)
         form = response.context.get('form1')
         self.assertIsInstance(form, NewTopicForm)
 
     def test_new_topic_valid_post_data(self):
-        url = reverse('new_topic', kwargs={'board_pk': 1})
+        url = reverse('new_topic', kwargs={'board_pk': self.board_pk})
         data = {'topic_subject': 'New topic',
                 'post_subject': 'Qude vadem temple',
                 'message': 'Lorem ipsum dolor sit amet'
@@ -69,7 +80,7 @@ class NewTopicTests(TestCase):
         Invalid post data should not redirect
         The expected behavior is to show the form again with validation errors
         '''
-        url = reverse('new_topic', kwargs={'board_pk': 1})
+        url = reverse('new_topic', kwargs={'board_pk': self.board_pk})
         response = self.client.post(url, {})
         form = response.context.get('form1')
         self.assertEqual(response.status_code, 200)
@@ -80,7 +91,7 @@ class NewTopicTests(TestCase):
         Invalid post data should not redirect
         The expected behavior is to show the form again with validation errors
         '''
-        url = reverse('new_topic', kwargs={'board_pk': 1})
+        url = reverse('new_topic', kwargs={'board_pk': self.board_pk})
         data = {
             'subject': '',
             'message': ''
--- howdimain/settings.py
+++ howdimain/settings.py
@@ -84,22 +84,15 @@ WSGI_APPLICATION = 'howdimain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#                 },
-#         }
-
-DATABASES = {            ### USE THIS FOR TESTING
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+        },
 }
 
 # Global martor settings

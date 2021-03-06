0359ab78f49af533cdd810da136b5f3cc4a756b8
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2021-03-06 11:45:32 +0700

    howdidesk

--- howdidesk.py
+++ howdidesk.py
@@ -1,20 +1,11 @@
 import os
-import time
 import webview
-from threading import Thread
 
 def start_webview():
     window = webview.create_window(
-        'Howdimain', url='http://127.0.0.1:8000', confirm_close=True, width=900, height=600)
+        'Howdimain', url='https://www.howdiweb.nl', confirm_close=True, width=1000, height=600)
     webview.start()
     window.closed = os._exit(0)
 
-def start_django():
-    local_host = '127.0.0.1'
-    port =  '8000'
-    os.system(f'python manage.py runserver {local_host}:{port}')
-    time.sleep(5)
-
 if __name__ == '__main__':
-    Thread(target=start_django).start()
     start_webview()

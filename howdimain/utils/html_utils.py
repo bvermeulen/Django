5626b31265ff58ec43aa4ff0f0e7986a46868375
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2021-06-29 18:07:02 +0700

    add tests for app music

--- howdimain/utils/html_utils.py
+++ howdimain/utils/html_utils.py
@@ -2,6 +2,8 @@ import re
 import html
 
 def convert_string_to_html(a_string):
-    return re.sub(r'&#(x[\dA-F]+);',
-                  lambda m: '&#' + str(int('0' + m.group(1), 16)) + ';',
-                  html.escape(a_string, quote=True))
+    return html.escape(a_string, quote=True)
+
+    # return re.sub(r'&#(x[\dA-F]+);',
+    #               lambda m: '&#' + str(int('0' + m.group(1), 16)) + ';',
+    #               html.escape(a_string, quote=True))
--- music/tests/test_music_views.py
+++ music/tests/test_music_views.py
@@ -1,23 +1,36 @@
 from django.contrib.auth.models import User
 from django.urls import reverse, resolve
 from django.test import TestCase
-from ..models import PlayList
-from ..views import PlayTopTracksView
+from ..models import MusicTrack
+from ..views import PlayTopTracksView, PlayListView
+
 
 class MusicTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        testuser = User.objects.create(
+        testuser = User.objects.create_user(
             username='testuser',
             email='testuser@aa.com',
+            first_name='test_is_my_name',
             password='testuser_pw'
         )
+        _ = MusicTrack.objects.create(
+            user=testuser,
+            track_id='1zwMYTA5nlNjZxYrvBB2pV',
+            track_artist='Adele',
+            track_name='Someone Like You',
+        )
 
-class PlayTopTracksView(MusicTests):
+
+class TestPlayTopTracksView(MusicTests):
 
     def setUp(self):
         self.response = self.client.post(reverse('play_top_tracks'))
+
+    def test_get_placeholder(self):
+        response = self.client.get(reverse('play_top_tracks'))
+        self.assertContains(response, 'placeholder="enter name artist ..."')
 
     def test_csrf_token(self):
         self.assertContains(self.response, 'csrfmiddlewaretoken')
@@ -26,19 +39,87 @@ class PlayTopTracksView(MusicTests):
         self.assertEqual(self.response.status_code, 200)
 
     def test_url_resolves(self):
-        view = resolve('/play_top_tracks/')
-        self.assertEqual(view.func, PlayTopTracksView.as_view())
+        view = resolve('/music/play_top_tracks/')
+        self.assertEqual(view.func.__name__, PlayTopTracksView.__name__)
 
     def test_view_contains_link_to_home_page(self):
         home_url = reverse('home')
         self.assertContains(self.response, f'href="{home_url}"')
 
-    def test_artist_query(self):
+    def test_valid_artist_query(self):
         data = {'artist_query': 'Adele'}
         response = self.client.post(reverse('play_top_tracks'), data)
-        self.assertEqual('2', response.context['status'][11:12])
+        self.assertGreater(len(response.context['artist']['top_tracks']), 0)
+        self.assertContains(response, 'placeholder="Adele"')
 
-    def test_track(self):
-        data = {'track': '1234567890'}
+    def test_invalid_artist_query(self):
+        data = {'artist_query': '_Adele'}
         response = self.client.post(reverse('play_top_tracks'), data)
-        self.assertEqual('2', response.context['status'][11:12])
+        self.assertEqual(len(response.context['artist']['top_tracks']), 0)
+        self.assertContains(response, 'placeholder="_Adele"')
+
+    def test_with_login(self):
+        self.client.login(username='testuser', password='testuser_pw')
+        data = {'artist_query': 'Adele'}
+        response = self.client.post(reverse('play_top_tracks'), data)
+        playlist_url = reverse('playlist')
+        self.assertContains(response, f'href="{playlist_url}"')
+        self.assertContains(response, 'add to playlist')
+
+    def test_view_adds(self):
+        self.client.login(username='testuser', password='testuser_pw')
+        data = {'artist_query': '', 'track_id': '7xdLNxZCtY68x5MAOBEmBq'}
+        _ = self.client.post(reverse('play_top_tracks'), data)
+        self.assertEqual(MusicTrack.objects.all()[1].track_name, 'All Along the Watchtower')
+
+    def test_with_anomynous(self):
+        data = {'artist_query': 'Adele'}
+        response = self.client.post(reverse('play_top_tracks'), data)
+        playlist_url = reverse('playlist')
+        self.assertNotContains(response, f'href="{playlist_url}"')
+        self.assertNotContains(response, 'add to playlist')
+
+
+class TestPlayListView(MusicTests):
+
+    def setUp(self):
+        self.client.login(username='testuser', password='testuser_pw')
+        # note post redirects to get
+        self.response = self.client.get(reverse('playlist'))
+
+    def test_playlist_view_not_logged_in_redirects_to_login_page(self):
+        self.client.logout()
+        response = self.client.post(reverse('playlist'))
+        login_url = reverse('login') + f'?next={reverse("playlist")}'
+        self.assertRedirects(response, login_url, fetch_redirect_response=False)
+        self.assertEqual(response.status_code, 302)
+
+    def test_csrf_token(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_view_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_url_resolves(self):
+        view = resolve('/music/playlist/')
+        self.assertEqual(view.func.__name__, PlayListView.__name__)
+
+    def test_view_contains_link_to_toptracks(self):
+        tracks_url = reverse('play_top_tracks')
+        self.assertContains(self.response, f'href="{tracks_url}"')
+
+    def test_view_contains_link_to_home_page(self):
+        home_url = reverse('home')
+        self.assertContains(self.response, f'href="{home_url}"')
+
+    def test_view_contains_user_first_name(self):
+        self.assertContains(self.response, 'Playlist test_is_my_name')
+
+    def test_view_contains_delete(self):
+        self.assertContains(self.response, 'delete from playlist')
+
+    def test_view_deletes(self):
+        track_pk = MusicTrack.objects.all()[0].pk
+        data = {'track_pk': track_pk}
+        _ = self.client.post(reverse('playlist'), data)
+        self.assertEqual(len(MusicTrack.objects.all()), 0)

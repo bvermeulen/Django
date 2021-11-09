1c9c1bb816bf1b754a2b24e0bb5b0aeed7e5e050
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2021-11-09 18:00:04 +0700

    refactor stock_quotes

--- stock/views/quotes.py
+++ stock/views/quotes.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
 from stock.forms import StockQuoteForm
 from stock.models import Person, Portfolio
@@ -36,10 +36,12 @@ class QuoteView(View):
             default_user = None
 
         quote_string = request.session.get('quote_string', '')
+        selected_portfolio = request.session.get('selected_portfolio', '')
         markets = request.session.get('markets', self.markets)
         stockdetail = request.session.get('stockdetail', STOCK_DETAILS[0][0])
         form = self.stockquote_form(initial={
             'quote_string': quote_string,
+            'selected_portfolio': selected_portfolio,
             'markets': markets,
             'stockdetails': stockdetail,
         })
@@ -48,8 +50,38 @@ class QuoteView(View):
         if user.is_authenticated:
             portfolios += user.get_portfolio_names()
 
+        if selected_portfolio:
+            try:
+                # try if user has selected a portfolio if authenticated
+                if user.is_authenticated:
+                    symbols = Portfolio.objects.get(
+                        user=user, portfolio_name=selected_portfolio).get_stock()
+                    stock_info = self.td.get_stock_trade_info(symbols[0:20])
+                    stock_info += self.td.get_stock_trade_info(symbols[20:40])
+
+                else:
+                    raise Portfolio.DoesNotExist
+
+            except Portfolio.DoesNotExist:
+                # try if it is a default portfolio
+                try:
+                    symbols = Portfolio.objects.get(
+                        user=default_user, portfolio_name=selected_portfolio).get_stock()  #pylint: disable=line-too-long
+                    stock_info = self.td.get_stock_trade_info(symbols[0:20])
+                    stock_info += self.td.get_stock_trade_info(symbols[20:40])
+
+                except Portfolio.DoesNotExist:
+                    pass
+
+        else:
+            symbols = self.td.parse_stock_quote(quote_string, markets=markets)
+            stock_info = self.td.get_stock_trade_info(symbols[0:20])
+
+        stock_info = add_display_tokens(stock_info)
+        stock_info = format_and_sort_stocks(stock_info)
+
         context = {'source': source,
-                   'stock_info': [],
+                   'stock_info': stock_info,
                    'form': form,
                    'portfolios': sorted(portfolios),
                    'data_provider_url': self.data_provider_url, }
@@ -69,66 +101,37 @@ class QuoteView(View):
             default_user = None
 
         quote_string = request.session.get('quote_string', '')
+        selected_portfolio = request.session.get('selected_portfolio', '')
         markets = request.session.get('markets', self.markets)
         stockdetail = request.session.get('stockdetail', STOCK_DETAILS[0][0])
 
         form = self.stockquote_form(request.POST)
         if form.is_valid():
             form_data = form.cleaned_data
-            quote_string = form_data.get('quote_string')
-            selected_portfolio = form_data.get('selected_portfolio')
+            new_quote_string = form_data.get('quote_string')
+            new_selected_portfolio = form_data.get('selected_portfolio')
             markets = form_data.get('markets')
             stockdetail = form_data.get('stockdetails')
-            symbols = []
-            stock_info = []
 
-            if selected_portfolio:
-                try:
-                    # try if user has selected a portfolio if authenticated
-                    if user.is_authenticated:
-                        symbols = Portfolio.objects.get(
-                            user=user, portfolio_name=selected_portfolio).get_stock()
-                        stock_info = self.td.get_stock_trade_info(symbols[0:20])
-                        stock_info += self.td.get_stock_trade_info(symbols[20:40])
+            if new_selected_portfolio != selected_portfolio:
+                selected_portfolio = new_selected_portfolio
+                quote_string = ''
 
-                    else:
-                        raise Portfolio.DoesNotExist
-
-                except Portfolio.DoesNotExist:
-                    # try if it is a default portfolio
-                    try:
-                        symbols = Portfolio.objects.get(
-                            user=default_user, portfolio_name=selected_portfolio).get_stock()  #pylint: disable=line-too-long
-                        stock_info = self.td.get_stock_trade_info(symbols[0:20])
-                        stock_info += self.td.get_stock_trade_info(symbols[20:40])
-
-                    except Portfolio.DoesNotExist:
-                        pass
+            elif new_quote_string != quote_string:
+                quote_string = new_quote_string
+                selected_portfolio = ''
 
             else:
-                symbols = self.td.parse_stock_quote(quote_string, markets=markets)
-                stock_info = self.td.get_stock_trade_info(symbols[0:20])
+                pass
 
             request.session['quote_string'] = quote_string
+            request.session['selected_portfolio'] = selected_portfolio
             request.session['markets'] = markets
             request.session['stockdetail'] = stockdetail
             logger.info(f'user {user} [ip: {get_client_ip(request)}] looking '
                         f'up: {quote_string} / {selected_portfolio}')
 
         else:
-            stock_info = []
+            pass
 
-        portfolios = default_user.get_portfolio_names()
-
-        if user.is_authenticated:
-            portfolios += user.get_portfolio_names()
-
-        stock_info = add_display_tokens(stock_info)
-        stock_info = format_and_sort_stocks(stock_info)
-        context = {'source': source,
-                   'stock_info': stock_info,
-                   'form': form,
-                   'portfolios': sorted(portfolios),
-                   'data_provider_url': self.data_provider_url, }
-
-        return render(request, self.template_name, context)
+        return redirect(reverse('stock_quotes'))

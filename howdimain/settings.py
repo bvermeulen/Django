ada841c3145e59fea085cd4e84867937c58c89d2
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2021-11-09 15:08:35 +0700

    add stock news (update settings)

--- howdimain/settings.py
+++ howdimain/settings.py
@@ -17,7 +17,7 @@ logformat = '%(asctime)s:%(levelname)s:%(message)s'
 Logger.set_logger(config('LOG_FILE'), logformat, 'INFO')
 logger = Logger.getlogger()
 HOWDIMAIN_VERSION = 'https://github.com/bvermeulen/Django/tree/howdimain-digitalocean_v9'
-HOWDIMAIN_DATE = 'June, 2021: add music app'
+HOWDIMAIN_DATE = 'November, 2021: add stock news'
 HOWDIMAIN_AUTHOR = 'bruno.vermeulen@hotmail.com'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)

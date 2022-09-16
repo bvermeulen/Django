be1763a29fe374125ecd92b4cd41458de71ccfaf
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2022-09-16 07:58:38 +0700

    Django 4: add CSRF_TRUSTED_ORIGINS

--- howdimain/settings.py
+++ howdimain/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = [f'https://{domain}' for domain in ALLOWED_HOSTS]
 
 # Application definition
 

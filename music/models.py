57b70b10a41014b5ccb8ae657eca236d49b2d997
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2024-11-11 23:51:44 +0100

    music: add audio preview

--- music/models.py
+++ music/models.py
@@ -6,14 +6,17 @@ from django.db.models.deletion import CASCADE
 
 class MusicTrack(models.Model):
     track_id = models.CharField(max_length=30)
-    track_artist = models.CharField(max_length=100)
-    track_name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    artist = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    preview_url = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'user: {self.user}: {self.track_id}, {self.track_artist}, {self.track_name}'
+        return f"user: {self.user}: {self.track_id}, {self.artist}, {self.name}"
 
     class Meta:
-        unique_together = ['track_id', 'user']
-
+        unique_together = ["track_id", "user"]
--- music/views.py
+++ music/views.py
@@ -15,63 +15,66 @@ from spotipy.exceptions import SpotifyException
 logger = Logger.getlogger()
 
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
-    client_id=SPOTIFY_CLIENT_ID,
-    client_secret=SPOTIFY_CLIENT_SECRET,
-))
+spotify = spotipy.Spotify(
+    client_credentials_manager=SpotifyClientCredentials(
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+    )
+)
+
 
 class PlayTopTracksView(View):
-    template_name = 'music/play_top_tracks.html'
-    artist_empty = {'artist': 'enter name artist ...', 'top_tracks': []}
+    template_name = "music/play_top_tracks.html"
+    artist_empty = {"artist": "enter name artist ...", "top_tracks": []}
 
     def get(self, request):
-        artist = request.session.get('artist', self.artist_empty)
-        if not artist.get('top_tracks'):
+        artist = request.session.get("artist", self.artist_empty)
+        if not artist.get("top_tracks"):
             artist = self.artist_empty
 
-        context = {
-            'artist': artist
-        }
+        context = {"artist": artist}
         return render(request, self.template_name, context)
 
     def post(self, request):
         user = request.user
-        artist = request.session.get('artist', self.artist_empty)
+        artist = request.session.get("artist", self.artist_empty)
 
-        if request.method=='POST':
-            artist_query = request.POST.get('artist_query')
-            track_id = request.POST.get('track_id')
+        if request.method == "POST":
+            artist_query = request.POST.get("artist_query")
+            track_id = request.POST.get("track_id")
 
-            if artist_query and artist_query != artist.get('artist'):
+            if artist_query and artist_query != artist.get("artist"):
                 artist_name = artist_query
                 top_tracks = []
                 try:
-                    artists = spotify.search(q='artist:' + artist_query, type='artist')
+                    artists = spotify.search(q="artist:" + artist_query, type="artist")
                     top_tracks = spotify.artist_top_tracks(
-                        artists['artists']['items'][0]['uri'][15:]
-                    )['tracks'][:10]
+                        artists["artists"]["items"][0]["uri"][15:]
+                    )["tracks"][:10]
 
                 except Exception as e:
                     pass
 
                 artist = {
-                    'artist': artist_name,
-                    'top_tracks': top_tracks,
+                    "artist": artist_name,
+                    "top_tracks": top_tracks,
                 }
-                request.session['artist'] = artist
+                request.session["artist"] = artist
 
             elif user.is_authenticated and track_id:
                 try:
                     track_data = spotify.track(track_id)
                     MusicTrack.objects.create(
-                        track_id=track_data.get('id'),
-                        track_artist=track_data.get('artists')[0].get('name')[:100],
-                        track_name=track_data.get('name')[:100],
+                        track_id=track_data.get("id"),
+                        artist=track_data.get("artists")[0].get("name")[:100],
+                        name=track_data.get("name")[:100],
+                        preview_url=track_data.get("preview_url", "")[:100],
                         user=user,
                     )
                     logger.info(
-                        f'user {user} [ip: {get_client_ip(request)}] '
-                        f'added {track_data.get("name")} to playlist')
+                        f"user {user} [ip: {get_client_ip(request)}] "
+                        f"added {track_data.get("name")} to playlist"
+                    )
 
                 except (IntegrityError, SpotifyException):
                     pass
@@ -83,40 +86,38 @@ class PlayTopTracksView(View):
             pass
 
         context = {
-            'artist': artist,
+            "artist": artist,
         }
 
         return render(request, self.template_name, context)
 
 
-@method_decorator(login_required, name='dispatch')
+@method_decorator(login_required, name="dispatch")
 class PlayListView(View):
-    template_name = 'music/playlist.html'
+    template_name = "music/playlist.html"
 
     def get(self, request):
         user = request.user
         track_list = list(MusicTrack.objects.filter(user=user))
         random.shuffle(track_list)
 
-        context = {
-            'track_list': track_list
-        }
+        context = {"track_list": track_list}
         return render(request, self.template_name, context)
 
     def post(self, request):
         user = request.user
 
-        if request.method=='POST':
-            track_pk = request.POST.get('track_pk')
+        if request.method == "POST":
+            track_pk = request.POST.get("track_pk")
             try:
                 track_to_be_deleted = MusicTrack.objects.get(pk=track_pk)
                 logger.info(
-                    f'user {user} [ip: {get_client_ip(request)}] '
-                    f'removed {track_to_be_deleted.track_name} from playlist'
+                    f"user {user} [ip: {get_client_ip(request)}] "
+                    f"removed {track_to_be_deleted.name} from playlist"
                 )
                 track_to_be_deleted.delete()
 
             except MusicTrack.DoesNotExist:
                 pass
 
-        return redirect(reverse('playlist'))
+        return redirect(reverse("playlist"))

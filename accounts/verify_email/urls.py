3b27fb81a8d38c040a9917cbc742d148377fccd6
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2026-04-12 22:21:27 +0700

    verify email - add a landing page

--- accounts/verify_email/urls.py
+++ accounts/verify_email/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
-from .views import verify_user_and_activate, request_new_link
+from .views import verify_user_and_activate, confirm_email, request_new_link
 
 urlpatterns = [
-    path(f'user/verify-email/<useremail>/<usertoken>/', verify_user_and_activate, name='verify-email'),
-    path(f'user/verify-email/request-new-link/<useremail>/<usertoken>/', request_new_link, name='request-new-link-from-token'),
-    path(f'user/verify-email/request-new-link/', request_new_link, name='request-new-link-from-email'),
+    path(
+        f"user/verify-email/<useremail>/<usertoken>/",
+        verify_user_and_activate,
+        name="verify-email",
+    ),
+    path(
+        f"user/confirm-email/<useremail>/<usertoken>/",
+        confirm_email,
+        name="confirm-email"
+    ),
+    path(
+        f"user/verify-email/request-new-link/<useremail>/<usertoken>/",
+        request_new_link,
+        name="request-new-link-from-token",
+    ),
+    path(
+        f"user/verify-email/request-new-link/",
+        request_new_link,
+        name="request-new-link-from-email",
+    ),
 ]
